249f12bd8f8066b5d72045182a1315798b52053b
Jose Daniel <josdan018@gmail.com>
2011-07-07 13:21:01 -0430

    se coloco las propiedades (estaticas) de los enlaces de figura
    Signed-off-by: Jose Daniel <josdan018@gmail.com>

--- Semestre_2011_1/MVC/src/modelo/enlace.java
+++ Semestre_2011_1/MVC/src/modelo/enlace.java
@@ -1,16 +1,34 @@
 package modelo;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
 
 public class enlace extends Figura{
+	static int 
+		ENLACE_HORIZONTAL=1,
+		ENLACE_VERTICAL=2,
+		ENLACE_SOLIDO=1,
+		ENLACE_VOLATIL_CORRECTO=2,
+		ENLACE_VOLATIL_INCORRECTO=3
+		;
+	 
+	int ancho, alto, orientacion, estado;
 	
-	int ancho, alto;
-	
-	public enlace(Point posicion, int ancho, int alto) {
+	public enlace(Point posicion, int ancho, int orientacion, int estado) {
 		this.posicion=posicion;
-		this.alto=alto;
-		this.ancho=ancho;
+		this.orientacion=orientacion;
+		this.estado=estado;
+		if(orientacion==ENLACE_VERTICAL){
+			this.ancho=ancho/8;
+			this.alto=ancho;
+		}else if(orientacion==ENLACE_HORIZONTAL){
+			this.alto=ancho/8;
+			this.ancho=ancho;
+		}
+		
 		
 		// TODO Auto-generated constructor stub
 	}
@@ -25,13 +43,20 @@ public class enlace extends Figura{
 	
 	@Override
 	public boolean dentroFigura(Point p) {
-		// TODO Auto-generated method stub
-		return (true);
+		
+		return new Rectangle(posicion,new Dimension(ancho, alto)).contains(p);
 	}
 
 	@Override
 	public void dibujar(Graphics g) {
-		g.drawRect(posicion.x,posicion.y,ancho,alto);
+		if(estado==ENLACE_SOLIDO)
+			g.setColor(Color.BLACK);
+		else if(estado==ENLACE_VOLATIL_CORRECTO)
+			g.setColor(Color.GREEN);
+		else if(estado==ENLACE_VOLATIL_INCORRECTO)
+			g.setColor(Color.RED);
+		 
+		g.fillRect(posicion.x,posicion.y,ancho,alto);
 		// TODO Auto-generated method stub
 		
 	}
