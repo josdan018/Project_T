0314ab738c5bdc33aa3d5b3faa036f90de84fe5b
Zulma <zulmadessire@gmail.com>
2011-07-24 21:29:59 -0400

    Metodo cuadro_informacion terminado!!!!!... pero sin pruebas

--- Semestre_2011_1/MVC/src/Figuras.java
+++ Semestre_2011_1/MVC/src/Figuras.java
@@ -47,6 +47,13 @@ public class Figuras {
 			Vista vista = new Vista(new Dimension(1000,800),modelo);
 			JPanel b=new JPanel(),c=new JPanel(),d=new JPanel();
 			
+			JPanel a=new JPanel();
+			JTextArea t_area = new JTextArea("  . . . . . Creando \n",10,80); 
+			a.setOpaque(true);
+			a.setBackground(Color.BLUE);
+			a = new JPanel(new FlowLayout());
+	        a.add(new JLabel("Acción Realizada:  ",10));
+			
 			b.setOpaque(true);
 			b.setBackground(Color.GREEN);
 			b.add(new JButton("Listo"));
@@ -60,13 +67,18 @@ public class Figuras {
 			d.setOpaque(true);
 			d.setBackground(Color.RED);
 			
-			cuadro_informacion(guiobjects, p);
+			
 			
 			final Controlador controlador = new Controlador(modelo,vista);
 			vista.controlador=controlador; //Lo registro para su uso, deberia ser un metodo pero por simplificacion
 			JScrollPane ModelScroll = new JScrollPane(controlador.getVista(), ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 			guiobjects.add(ModelScroll,BorderLayout.CENTER);
+		
+			//cuadro_informacion(p, t_area);
 			
+			a.add(t_area);
+			
+			guiobjects.add(a,BorderLayout.SOUTH);
 			guiobjects.add(b,BorderLayout.WEST);
 			guiobjects.add(c,BorderLayout.EAST);
 			
@@ -92,54 +104,102 @@ public class Figuras {
 		   System.exit(0);
         }
 	
-	public static void cuadro_informacion (Container guiobjects, Vector<pieza> p){
-		
-		
-		JPanel a=new JPanel();
-		JTextArea t_area = new JTextArea("  . . . . . Creando Compilador \n",10,80); 
-		a.setOpaque(true);
-		a.setBackground(Color.BLUE);
-		a = new JPanel(new FlowLayout());
-        a.add(new JLabel("Acción Realizada:  ",10));
-        
+	public static void cuadro_informacion ( Vector<pieza> p, JTextArea t_area){
         //Acciones
-        
+		int xx = 0;
+		
         if(p.elementAt(0).getIdentificador()== tipoPieza.COMPILADOR){
         	
-        	t_area.setText(
-        			t_area.getText()+
-        			" A partir de un compilador de: "+
-        					p.elementAt(0).getCuadrados().elementAt(1).getNombre()+
-        					" , a:"+p.elementAt(0).getCuadrados().elementAt(2).getNombre()+
-        					", escrito en: "+
-        					p.elementAt(0).getCuadrados().elementAt(3).getNombre());
+        	cuadro_informacion_compilador(p, t_area, 0);
         	
         	if(p.elementAt(1).getIdentificador()== tipoPieza.COMPILADOR){
             	
-            	t_area.setText(
-            			t_area.getText()+
-            			" A partir de un compilador de: "+
-            					p.elementAt(1).getCuadrados().elementAt(1).getNombre()+
-            					" , a:"+p.elementAt(1).getCuadrados().elementAt(2).getNombre()+
-            					", escrito en: "+
-            					p.elementAt(1).getCuadrados().elementAt(3).getNombre());
+            	cuadro_informacion_compilador(p, t_area, 1);
             	
-            		if(p.elementAt(2).getIdentificador()== tipoPieza.COMPILADOR){
-                	
-                	t_area.setText(
-                			t_area.getText()+
-                			" A partir de un compilador de: "+
-                					p.elementAt(2).getCuadrados().elementAt(1).getNombre()+
-                					" , a:"+p.elementAt(2).getCuadrados().elementAt(2).getNombre()+
-                					", escrito en: "+
-                					p.elementAt(2).getCuadrados().elementAt(3).getNombre());
-            		}
+            	if(p.elementAt(2).getIdentificador()== tipoPieza.MAQUINA){
+            		cuadro_informacion_maquina(p, t_area, 2);
+            	}
             }
+        	else if (p.elementAt(1).getIdentificador()== tipoPieza.INTERPRETE){
+        		cuadro_informacion_interprete (p, t_area, 1);
+        		xx = 2;
+        		while(p.elementAt(xx).getIdentificador()== tipoPieza.INTERPRETE){
+        			cuadro_informacion_interprete (p, t_area, xx);
+        			xx = xx + 1;
+        		}
+        		if(p.elementAt(xx).getIdentificador()== tipoPieza.COMPILADOR){
+                	
+                	cuadro_informacion_compilador(p, t_area, xx);
+        		}
+        	}
+        	
+        }
+        else if(p.elementAt(0).getIdentificador()== tipoPieza.PROGRAMA){
+        	cuadro_informacion_compilador(p, t_area,0);
+        	if(p.elementAt(1).getIdentificador()== tipoPieza.COMPILADOR){
+            	
+            	cuadro_informacion_compilador(p, t_area,1);
+            	
+            	if(p.elementAt(2).getIdentificador()== tipoPieza.MAQUINA){
+            		cuadro_informacion_maquina(p, t_area, 2);
+            	}
+            }
+        	else if (p.elementAt(1).getIdentificador()== tipoPieza.INTERPRETE){
+        		cuadro_informacion_interprete (p, t_area, 1);
+        		xx = 2;
+        		while(p.elementAt(xx).getIdentificador()== tipoPieza.INTERPRETE){
+        			
+        			cuadro_informacion_interprete (p, t_area, xx);
+        			xx = xx + 1;
+        		}
+        		
+        		if(p.elementAt(xx).getIdentificador()== tipoPieza.MAQUINA){
+        			cuadro_informacion_maquina(p, t_area, xx);
+        		}
+        	}
+        	
         }
         
-        a.add(t_area);
-	
-        guiobjects.add(a,BorderLayout.SOUTH);
+        
 	}
+	public static void cuadro_informacion_compilador (Vector<pieza> p, JTextArea t_area, int i){
+		
+		t_area.setText(
+    			t_area.getText()+
+    			" A partir de un compilador de: "+
+    					p.elementAt(i).getCuadrados().elementAt(1).getNombre()+
+    					" , a:"+p.elementAt(i).getCuadrados().elementAt(2).getNombre()+
+    					", escrito en: "+
+    					p.elementAt(i).getCuadrados().elementAt(3).getNombre());
+    	
+		
+	}
+	
+	public static void cuadro_informacion_maquina (Vector<pieza> p, JTextArea t_area, int i){
+		
+		t_area.setText(
+    			t_area.getText()+
+    			" Ejecutado en la Maquina: "+
+    					p.elementAt(i).getCuadrados().elementAt(0).getNombre());
+	}
+	public static void cuadro_informacion_interprete (Vector<pieza> p, JTextArea t_area, int i){
+		
+		t_area.setText(
+    			t_area.getText()+
+    			" Interprete de: "+
+    					p.elementAt(i).getCuadrados().elementAt(0).getNombre()+
+    					", para: "+p.elementAt(i).getCuadrados().elementAt(1).getNombre());
+	}
+	
+	public static void cuadro_informacion_programa (Vector<pieza> p, JTextArea t_area, int i){
+		
+		t_area.setText(
+    			t_area.getText()+
+    			" El programa: "+
+    					p.elementAt(i).getCuadrados().elementAt(0).getNombre()+
+    					", Escrito en el lenguaje: "+p.elementAt(i).getCuadrados().elementAt(1).getNombre());
+	}
+	
+	
 
 }
